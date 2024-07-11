f1024a8579d7a3025666b3f5e66cd1a38e810456
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-11 20:22:53 +0300

    models, relationships

---
+++ app/Models/Media.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_media');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
