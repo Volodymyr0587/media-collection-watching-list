5df9b50083c184d85cd25b45d7c841968ea7d36e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-11 22:17:02 +0300

    delete category

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -42,12 +42,11 @@
                         </div>
 
                         <div class="mt-6 flex items-center justify-end gap-x-6">
-                            <a href="{{ route("categories.index") }}" class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Cancel</a>
+                                <a href="{{ route("categories.index") }}" class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Cancel</a>
                             <button type="submit"
                                 class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
                         </div>
                     </form>
-
                 </div>
             </div>
         </div>
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -10,7 +10,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 @auth
                 <div class="p-6">
-                    <a href="{{ route('categories.create') }}">{{ __('Create Category') }}</a>
+                    <a href="{{ route('categories.create') }}"
+                    class="mt-2 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __('Create Category') }}</a>
                 </div>
                 @endauth
                 <div class="p-6 text-gray-900">
@@ -33,8 +34,16 @@
                                     <tr class="border-b border-neutral-200 dark:border-white/10">
                                         <td class="whitespace-nowrap px-6 py-4 font-medium">{{ $loop->iteration }}</td>
                                         <td class="whitespace-nowrap px-6 py-4">{{ $category->name }}</td>
-                                        <td class="whitespace-nowrap px-6 py-4">
+                                        <td class="whitespace-nowrap px-6 py-4 flex items-center gap-2">
                                             <a href="{{ route("categories.edit", $category) }}" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Edit</a>
+                                            @can('editCategory', $category)
+                                            <form action="{{ route('categories.destroy', $category) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" onclick="return confirm('Are you sure you want to delete this category?');"
+                                                    class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+                                            </form>
+                                            @endcan
                                         </td>
                                       </tr>
                                     @empty
