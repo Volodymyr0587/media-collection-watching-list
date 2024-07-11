27c38b368e649c94973d76e09ae6199f18464ae7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-11 22:19:57 +0300

    add Create Media button

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -8,9 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                @auth
+                <div class="p-6">
+                    <a href="{{ route('media.create') }}"
+                    class="mt-2 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ __('Create Media') }}</a>
                 </div>
+                @endauth
             </div>
         </div>
     </div>
