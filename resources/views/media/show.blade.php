31cc33d9fc81d2b5b27a0bdd72048e0192ed8de7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-17 20:05:42 +0300

    select all episodes checkbox

--- resources/views/media/show.blade.php
+++ resources/views/media/show.blade.php
@@ -83,6 +83,14 @@
                         @csrf
                         @method('PATCH')
                         <div class="mb-4 ml-4 flex flex-wrap">
+
+                        <!-- Select All Checkbox -->
+                        <div class="w-full p-2">
+                            <input type="checkbox" id="select-all" class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary">
+                            <label for="select-all" class="inline-block ps-[0.15rem] hover:cursor-pointer">Select All Episodes</label>
+                        </div>
+
+                        <!-- Individual Episode Checkboxes -->
                         @for ($i = 1; $i <= $media->series; $i++)
                             <div class="w-1/2 sm:w-1/4 lg:w-1/6 p-2">
                                 <input type="checkbox" id="episode{{ $i }}" name="episodes[]" value="{{ $i }}"
@@ -102,4 +110,13 @@
             </div>
         </div>
     </div>
+
+    <script>
+        document.getElementById('select-all').addEventListener('change', function() {
+            const checkboxes = document.querySelectorAll('input[name="episodes[]"]');
+            for (const checkbox of checkboxes) {
+                checkbox.checked = this.checked;
+            }
+        });
+    </script>
 </x-app-layout>
